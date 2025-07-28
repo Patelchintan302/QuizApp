4f252b11974afea787ad4201b65ae398d0f526e8
Patelchintan302 <patelcg302@gmail.com>
2025-07-29 00:22:52 +0530

    making progress in app development

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.quiz
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz.ui.QuestionActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val startButton: Button = findViewById(R.id.btnStart)
+        val editTextName: EditText = findViewById(R.id.name)
+
+        startButton.setOnClickListener {
+            if(!editTextName.text.toString().isEmpty()) {
+                val intent = Intent(this, QuestionActivity::class.java).also{
+                    it.putExtra("name", editTextName.text.toString())
+                    startActivity(it)
+                    finish()
+                }
+            }else {
+                Toast.makeText(this, "please enter your name!!",Toast.LENGTH_SHORT).show()
+            }
         }
+
+
     }
 }
